ee8f734d1e2d6efe21ccbfe7e272c8bc0656315c
Abid Mehmood <amehmood@suse.de>
2019-08-01 13:14:22 +0200

    enable passing grains to start event based on 'start_event_grains' configuration parameter

--- salt/config/__init__.py
+++ salt/config/__init__.py
@@ -1279,6 +1279,7 @@ DEFAULT_MINION_OPTS = {
     'state_top_saltenv': None,
     'startup_states': '',
     'sls_list': [],
+    'start_event_grains': [],
     'top_file': '',
     'thoriumenv': None,
     'thorium_top': 'top.sls',
